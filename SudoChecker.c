8593638f0861fa02927e32e37bb256521092a9f0
James Ahfat <james.ahfat@gmail.com>
2018-09-13 15:29:29 -0600

    File Input Output working for hardcode and cmd linearguments

--- SudoChecker.c
+++ SudoChecker.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define max 9   //number of slots in a row/column
 #define true 1  //non-zero is true
@@ -10,7 +11,7 @@ Program takes an valid input file of foramt specified in the assignment. It will
 27 other worker threads to check the requirements for Sudoku Vailidity.
 */
 
-const char take[2] = "\\t";
+const char take[4] = " \\t";
 char line[50], *ptr = NULL;
 int i, j = 0;
 int sudoku[max][max];
@@ -27,16 +28,8 @@ typedef struct elements
     int rightColumn;
 } elements;
 
-int main(int argc, char **argv[])
-{
-    if(argc > 1){
-        char *file_name = argv[1];
-        open_read(file_name);
-        printSudo();
-    };
-};
 
-void open_read(const char* file_name){
+void open_read(char *file_name){
     char c;
     int current;
     FILE *fp = fopen(file_name, "r");
@@ -44,32 +37,45 @@ void open_read(const char* file_name){
     {
         perror("Error while opening the file: Does not exist.");
         exit(-1);
-    };
-    while(EOF != fscanf(fp, "%s", line)){
-        printf("\n Read Line: %s\n", line);
+    }
+    while(!feof(fp)){
+        fgets(line, 50, fp);
+        printf("Read Line: %s\n", line);
         ptr = strtok(line, take);
-        sudoku[i][j++] = ptr;
-        //printf(sudoku[i][j-1]);
+        sudoku[i][j++] = atoi(ptr);
+        printf("ptr: %d\n", atoi(ptr));
         while(NULL != (ptr = strtok(NULL, take))){
-            sudoku[i][j] = ptr;
-            //printf("inside");
-            //printf(sudoku[i][j]);
+            sudoku[i][j] = atoi(ptr);
+            printf("ptr: %d\n", sudoku[i][j]);
             j++;
-        };
+        }
         j = 0;
         i++;
-    };
+    }
     fclose(fp);
-};
+}
 
 void printSudo(){
-    for(int i = 0; i < 9; i++){
-        for(int j = 0; j<9; j++){
-            printf(sudoku[i][j]);
-        };
+    int i;
+    for(i = 0; i < 9; i++){
+        int j;
+        for(j = 0; j<9; j++){
+            printf("%d", sudoku[i][j]);
+        }
         printf("\n");
-    };
-};
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    char *file_name = "";
+    if(argc > 1){
+        file_name = argv[1];
+    } else { file_name = "SudokuPuzzle.txt"; }
+    open_read(file_name);
+    printSudo();
+}
+
 
 /* get thread id for tid array
 pthread_id_np_t   tid;
