b83be9dc3758b11ffc0510ce73b702e6324bf451
James Ahfat <james.ahfat@gmail.com>
2018-09-16 21:52:27 -0600

    Add Invalid Case, Finished Program

--- SudoChecker.c
+++ SudoChecker.c
@@ -5,6 +5,15 @@
 
 #define max 9   //number of slots in a row/column
 
+typedef enum { false, true}  bool;
+
+typedef struct elements
+{
+    int topRow;
+    int bottomRow;
+    int leftColumn;
+    int rightColumn;
+} elements;
 /*
 Program takes an valid input file of foramt specified in the assignment. It will create one parent thread which spawns
 27 other worker threads to check the requirements for Sudoku Vailidity.
@@ -14,19 +23,9 @@ const char take[4] = " \\t";
 int i, j, colc, rowc, subc = 0;     //counter for columns, rows, subs
 char line[50], *ptr = NULL;
 int sudoku[max][max];
-int columnsChecked[max];
-int rowsChecked[max];
-int subsChecked[max];
-
-typedef enum { false, true}  bool;
-typedef struct elements
-{
-    int topRow;
-    int bottomRow;
-    int leftColumn;
-    int rightColumn;
-} elements;
-
+bool columnsChecked[max];
+bool rowsChecked[max];
+bool subsChecked[max];
 
 void open_read(char *file_name){
     char c;
@@ -39,13 +38,13 @@ void open_read(char *file_name){
     }
     while(!feof(fp)){
         fgets(line, 50, fp);
-        //printf("Read Line: %s\n", line);
+        //printf("Read Line: %s\n", line);              // debug line read
         ptr = strtok(line, take);
         sudoku[i][j++] = atoi(ptr);
-        //printf("ptr: %d\n", atoi(ptr));
-        while(NULL != (ptr = strtok(NULL, take))){
+        //printf("ptr: %d\n", atoi(ptr));               //display what is at strtok ptr
+        while(NULL != (ptr = strtok(NULL, take))){      
             sudoku[i][j] = atoi(ptr);
-            //printf("ptr: %d\n", sudoku[i][j]);
+            //printf("ptr: %d\n", sudoku[i][j]);        //Display whats put in cell
             j++;
         }
         j = 0;
@@ -54,7 +53,12 @@ void open_read(char *file_name){
     fclose(fp);
 }
 
-void printSudo(){
+/**
+ * 
+ * Print Sudoku to make sure file read in correctly
+ * Not used in final submission
+ * */
+void printSudo(){   
     int i;
     for(i = 0; i < 9; i++){
         int j;
@@ -71,32 +75,75 @@ void *checkColumn( void *element){
     int bottom = data -> bottomRow;
     int left = data -> leftColumn;
     int right = data -> rightColumn;
-    int i;
-    for (i = 0; i <= max - 1; i++){
-        int cell = sudoku[i][left];
+    int slots[10] = {false};
+    long id = pthread_self();
+    int u;
+    for (u = top; u < bottom; u++){
+        int val = sudoku[u][left];
+        if(slots[val] != false){
+            printf("\n0x%lx TRow: %d, BRow: %d, LCol: %d, RCol: %d invalid!", id, top, bottom, left, right);
+            pthread_exit(NULL);
+        }else slots[val] = true;
     }
+    columnsChecked[left] = true;
+    printf("\n0x%lx TRow: %d, BRow: %d, LCol: %d, RCol: %d valid!", id, top, bottom, left, right);
+    pthread_exit(NULL);
 }
 
 void *checkRow( void *element){
     elements *data = (elements *) element;
-    int start = data -> leftColumn;
-    int end = data -> rightColumn;
-    int row = data ->topRow;
-    int i;
-    for (i = start; start <= end; start++){
-        int cell = sudoku[row][start];
+    int top = data -> topRow;
+    int bottom = data -> bottomRow;
+    int left = data -> leftColumn;
+    int right = data -> rightColumn;
+    bool slots[10] = {false};
+    long id = pthread_self();
+    int u;
+    for (u = left; u <= right; u++){
+        int val = sudoku[top][u];
+        //printf("\nCheck Value: %d", val);         //debug
+        if(slots[val] != false){
+            printf("\n0x%lx TRow: %d, BRow: %d, LCol: %d, RCol: %d invalid!", id, top, bottom, left, right);
+            pthread_exit(NULL);
+        }else {
+            slots[val] = true;
+        }
     }
+    rowsChecked[top] = true;
+    printf("\n0x%lx TRow: %d, BRow: %d, LCol: %d, RCol: %d valid!", id, top, bottom, left, right);
+    pthread_exit(NULL);
 }
 
 void *checkGrids ( void *element){
     elements *data = (elements *) element;
-    int start = data -> leftColumn;
-    int end = data -> rightColumn;
+    int left = data -> leftColumn;
+    int right = data -> rightColumn;
     int top = data ->topRow;
-    int bot = data -> bottomRow;
-    int i;
-    for (i = start; start <= end; start++){
-        int cell = sudoku[i][end];
+    int bottom = data -> bottomRow;
+    bool slots[10] = {false};
+    long id = pthread_self();
+    int y;
+    for (y = top; y <= bottom; y++){
+        int z;
+        for( z = left; z <= right; z++){
+            int val = sudoku[y][z];
+            if(slots[val] != false){
+            printf("\n0x%lx TRow: %d, BRow: %d, LCol: %d, RCol: %d invalid!", id, top, bottom, left, right);
+            pthread_exit(NULL);
+            }else {
+            slots[val] = true;
+            }
+        }
+    }
+    subsChecked[j++] = true;
+    printf("\n0x%lx TRow: %d, BRow: %d, LCol: %d, RCol: %d valid!", id, top, bottom, left, right);
+    pthread_exit(NULL);
+}
+
+void joinThreads(pthread_t threads[]){
+    int b;
+    for( b = 0; b< 9; b++){
+        pthread_join(threads[0], NULL);
     }
 }
 
@@ -110,41 +157,76 @@ int main(int argc, char *argv[])
     open_read(file_name);
     //printSudo();
 
-    pthread_t tid_row[max], tid_col[max], tid_subgrid[max];
-    //thread creation
+    pthread_t tid_row[max], tid_col[max], tid_subgrid[max], dummy[max];
+    
+    //thread creation and checking
+    int count = 0;
     int l;
     for(l = 0; l < 9; l++){
         int m;
         for (m = 0; m < 9; m++){
-            if( m%3 == 0 && m%3 == 0){
+            if( l%3 == 0 && m%3 == 0){
+                //count++;
                 elements *gridData = (elements *)malloc(sizeof(elements));
                 gridData -> topRow = l;
                 gridData -> bottomRow = l + 2;
                 gridData -> leftColumn = m;
                 gridData -> rightColumn = m + 2;
                 pthread_create(&tid_subgrid[subc++], NULL, checkGrids, gridData);
-                printf("%ld\n", tid_subgrid[subc-1]);
+                //printf("grid pass: %d tid: 0x%lx\n", count, tid_subgrid[subc-1]); //debug passes
             }
-            if(l = 0){
+            if(l == 0){
+                //count++;
                 elements *colData = (elements *)malloc(sizeof(elements));
                 colData -> topRow = l;
                 colData -> bottomRow = max - 1;
                 colData -> leftColumn = m;
                 colData -> rightColumn = m;
-                printf("colthread\n");
                 pthread_create(&tid_col[colc++], NULL, checkColumn, colData);
+                //printf("col pass: %d tid: 0x%lxd\n", count, tid_col[colc-1]); //debug passes
             }
-            if(m = 0){
+            if(m == 0){
+                //count++;
                 elements *rowData = (elements *)malloc(sizeof(elements));
                 rowData -> topRow = l;
                 rowData -> bottomRow = l;
                 rowData -> leftColumn = m;
                 rowData -> rightColumn = max - 1;
-                printf("rowthread\n");
                 pthread_create(&tid_row[rowc++], NULL, checkRow, rowData);
+                //printf("row pass: %d tid: 0x%lxd\n", count, tid_row[rowc-1]); //debug passes
             }
             
         }
+    }
+
+    joinThreads(tid_subgrid);
+    joinThreads(tid_col);
+    joinThreads(tid_row);
+
+    int f;
+    for(f = 0; f < 9; f++){
+        char *validity = "";
+        if(columnsChecked[f] == true){
+            validity = "valid";
+        }else validity = "invalid";
+        printf("\nSubgrid: 0x%lx %s", tid_subgrid[f], validity);
+    }
+
+
+    for(f = 0; f < 9; f++){
+        char *validity = "";
+        if(columnsChecked[f] == true){
+            validity = "valid";
+        }else validity = "invalid";
+        printf("\nColumn: 0x%lx %s", tid_col[f], validity);
+    }
+
+    for(f = 0; f < 9; f++){
+        char *validity = "";
+        if(columnsChecked[f] == true){
+            validity = "valid";
+        }else validity = "invalid";
+        printf("\nRow: 0x%lx %s", tid_row[f], validity);
     }
 }
 
@@ -152,4 +234,6 @@ int main(int argc, char *argv[])
 /* get thread id for tid array
 pthread_id_np_t   tid;
 tid = pthread_getthreadid_np();
+
+can also put pointer into pthread_id array
 */
